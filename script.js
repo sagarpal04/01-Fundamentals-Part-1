d2a1b332aec3e8f0f351d9ca960994ad6ae8eef4
sagarpal04 <sagarpal0402@gmail.com>
2024-03-05 22:35:11 +0530

    Taking Decisions: if / else Statements

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMIMark > BMIJohn;
-console.log(BMIMark, BMIJohn);
-console.log(markHigherBMI);
+const age = 19;
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);

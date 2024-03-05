e36da79cbc2ed2b377e29639de197529558f5613
sagarpal04 <sagarpal0402@gmail.com>
2024-03-05 20:08:13 +0530

    Challenge #1

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-
-const firstName = "Jonas";
-const lastName = "Schmedtmann";
-console.log(firstName + " " + lastName);
-
-// Assignment Operator
-let x = 10 + 5;
-x += 10;
-x *= 4;
-x += 1;
-x -= 1;
-x -= 1;
-console.log(x);
-
-// Comparison Operator
-console.log(ageJonas > ageSarah);
-console.log(ageSarah >= 18);
-
-const isFullAge = ageSarah >= 18;
-
-console.log(now - 1991 > now - 2018);
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn);
+console.log(markHigherBMI);

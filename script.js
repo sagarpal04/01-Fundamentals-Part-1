b8a657a9fec51bca3c615db91196286f3f0283bc
sagarpal04 <sagarpal0402@gmail.com>
2024-03-05 19:55:01 +0530

    Basic Operators

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-let age = 30;
-age = 31;
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
-const birthYear = 1991;
-// birthYear = 1990;
-// const job
-// let is block scope
-// var is function scope
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
 
-var job = "programmer";
-job = "teacher";
+// Assignment Operator
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x += 1;
+x -= 1;
+x -= 1;
+console.log(x);
 
-lastName = "Schmedtmann";
-console.log(lastName);
+// Comparison Operator
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);

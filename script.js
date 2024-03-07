29cc5815383c7ef01c26e4153dd8c9b2d2204644
sagarpal04 <sagarpal0402@gmail.com>
2024-03-07 06:58:37 +0530

    Challenge #3

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+// const scoreDolphins = (96, 108, 89) / 3;
+// const scoreKoalas = (88, 91, 110) / 3;
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else {
+//   console.log("Both win the trophy");
+// }
+
 const scoreDolphins = (96, 108, 89) / 3;
 const scoreKoalas = (88, 91, 110) / 3;
-if (scoreDolphins > scoreKoalas) {
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("Dolphins win the trophy");
-} else if (scoreDolphins < scoreKoalas) {
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
   console.log("Koalas win the trophy");
-} else {
+} else if (
+  scoreDolphins == scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
   console.log("Both win the trophy");
+} else {
+  console.log("No one wins the trophy");
 }

b3086a70da5d276972b0d00c5242d6d1e0e0b800
sagarpal04 <sagarpal0402@gmail.com>
2024-03-08 06:42:15 +0530

    The Conditional (Ternary) Operator

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-const day = "sunday";
-switch (day) {
-  case "monday":
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thrusday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Record videos");
-    break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thrusday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend :D");
+const age = 23;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
 } else {
-  console.log("Not a valid day!");
+  drink2 = "water 💧";
 }
+
+console.log(drink2);
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
